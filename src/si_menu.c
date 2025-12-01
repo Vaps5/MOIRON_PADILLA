6c57b96f6b385b3c7bbac005c30a59f8780bf5da
valmoiron <val.moiron@gmail.com>
2025-12-01 19:32:39 +0100

    Val 1/12

--- src/si_menu.c
+++ src/si_menu.c
@@ -1,1 +1,21 @@
-//si_menu.c
+#include <stdio.h>
+#include "../include/si_menu.h"
+#include "si_sdl.h"
+#include "si_sdl.c"
+
+void menu(Game *g)
+{
+  si_text_display(g, "SCORE<1> HI-SCORE SCORE<2>", 1, 1);
+  si_text_display(g, "0000     0000     0000", 3, 3);
+  si_text_display(g, "PLAY", 7, 13);
+  si_text_display(g, "SPACE  INVADERS", 10, 8);
+  si_text_display(g, "*SCORE ADVANCE TABLE*", 14, 3);
+  si_text_display(g, "CREDIT", 29, 11);
+}
+
+void game_over(Game *g)
+{
+  si_text_display(g, "SCORE<1> HI-SCORE SCORE<2>", 1, 1);
+  si_text_display(g, "0000     0000     0000", 3, 3);
+  si_text_display(g, "GAME OVER", 14, 8); // à modifier apres les tests
+}
